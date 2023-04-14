c20d0b50540a8629c5a39ed2f7d00df965ab4753
S0dlive <sandlive662@gmail.com>
2023-04-14 19:04:03 +0200

    feat(auth-model): I add auth models

---
+++ src/Backend/Models/Register.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class Register
+    {
+        public string Email { set; get; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+    }
+}
