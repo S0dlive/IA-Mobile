9045243256c0eaca07bffe5333ad13c9b7ac19c9
S0dlive <sandlive662@gmail.com>
2023-04-14 19:03:12 +0200

    feat(auth-controller): I have finished the auth controller with a login & register endpoint & auth with JWT token.

---
+++ src/Backend/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Backend.Models;
+using Backend.Services;
+using Microsoft.AspNetCore.DataProtection.Internal;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+[Route("auth/")]
+public class AuthController : Controller
+{
+    private readonly JwtService _jwtService;
+    private readonly UserManager<User> _userManager;
+    public AuthController(
+        UserManager<User> userManager,
+        JwtService jwtService)
+    {
+        _userManager = userManager;
+        _jwtService = jwtService;
+    }
+    [HttpPost("login")]
+    public Task<IActionResult> Login()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(
+        [FromBody]Register register)
+    {
+        var userIsExist = _userManager.Users.FirstOrDefault(t => t.Email == register.Email);
+
+        if (userIsExist == null)
+        {
+            var user = new User
+            {
+                FirstName = register.FirstName,
+                Email = register.Email,
+                Id = Guid.NewGuid().ToString(),
+                UserName = register.Username,
+                LastName = register.LastName
+
+            };
+            var result = await _userManager.CreateAsync(user,
+                register.Password);
+            if (result.Succeeded)
+            {
+                return Ok(new
+                {
+                    Message = "This is your token",
+                    Token = _jwtService.GenerateToken(user)
+                });
+            }
+            return BadRequest(result.Errors.ToString());
+        }
+
+        return BadRequest("the currently mail was actually used.");
+    }
+}
---
+++ src/Backend/Services/JwtService.cs
@@ -0,0 +1,32 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Backend.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Backend.Services;
+
+public class JwtService
+{
+    private IConfiguration _configuration;
+    public JwtService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public string GenerateToken(User user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        var claims = new[]
+        {
+            new Claim("sub",user.Id),
+        };
+        var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+            _configuration["Jwt:Audience"],
+            claims,
+            expires: DateTime.Now.AddMinutes(60),
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
